b34fed39a128e7b93eb5ed2700733cd3eda36722
Avantika-Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2022-01-27 14:52:32 +0530

    Add files via upload

---
+++ 25jan/greaternum.js
@@ -0,0 +1,15 @@
+//Question:-Check whether number is greater than or equal to 10 or not.
+
+var a=prompt("Enter a number");
+if(a>10)
+{
+alert("Number is greater than 10");
+}
+else if(a==10)
+{
+alert("Number is equal to 10");
+}
+else
+{
+alert("Number is not greater than 10");
+}
